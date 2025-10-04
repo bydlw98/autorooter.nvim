74551790a2d3b45d61d8c5f7c307b7f2b818d5f7
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2025-10-04 21:33:20 +0800

    feat(setup): allow users to configure root_markers

--- lua/autorooter.lua
+++ lua/autorooter.lua
@@ -1,10 +1,10 @@
-local opts = {
+local config = {
   root_markers = { "Makefile", ".git" },
 }
 
 ---@return string
 local function root()
-  local root_dir = vim.fs.root(0, opts.root_markers)
+  local root_dir = vim.fs.root(0, config.root_markers)
 
   if root_dir == nil then
     local filename = vim.api.nvim_buf_get_name(0)
@@ -22,7 +22,9 @@ end
 
 local M = {}
 
-function M.setup()
+function M.setup(opts)
+  config = vim.tbl_deep_extend("force", config, opts or {})
+
   vim.api.nvim_create_autocmd({ "VimEnter", "BufEnter" }, {
     group = vim.api.nvim_create_augroup("autorooter.nvim", { clear = true }),
     callback = function()
