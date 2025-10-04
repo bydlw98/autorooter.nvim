841b6c67a2a714f88b01b05fd5baa3127ea3151d
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2025-10-04 21:24:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# autorooter.nvim
---
+++ lua/autorooter.lua
@@ -0,0 +1,34 @@
+local opts = {
+  root_markers = { "Makefile", ".git" },
+}
+
+---@return string
+local function root()
+  local root_dir = vim.fs.root(0, opts.root_markers)
+
+  if root_dir == nil then
+    local filename = vim.api.nvim_buf_get_name(0)
+    root_dir = vim.fs.dirname(filename)
+  end
+
+  return root_dir
+end
+
+local function rooter()
+  local root_dir = root()
+  vim.cmd.cd(root_dir)
+  vim.notify("cwd: " .. root_dir)
+end
+
+local M = {}
+
+function M.setup()
+  vim.api.nvim_create_autocmd({ "VimEnter", "BufEnter" }, {
+    group = vim.api.nvim_create_augroup("autorooter.nvim", { clear = true }),
+    callback = function()
+      rooter()
+    end,
+  })
+end
+
+return M
